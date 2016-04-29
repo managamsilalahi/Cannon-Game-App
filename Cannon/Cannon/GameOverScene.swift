1db25afb12a899cf1df2036e36bc16234a1b05b6
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-29 13:56:23 +0700

    Class GameOverScene.

--- Cannon/Cannon/GameOverScene.swift
+++ Cannon/Cannon/GameOverScene.swift
@@ -13,8 +13,40 @@ class GameOverScene: SKScene {
     // configure GameOverScene
     
     init(size: CGSize, won: Bool, time: CFTimeInterval) {
-        
         super.init(size: size)
+        
+        self.backgroundColor = SKColor.whiteColor()
+        let greenColor = SKColor(red: 0.0, green: 0.6, blue: 0.0, alpha: 1.0)
+        
+        let gameOverLabel = SKLabelNode(fontNamed: "Chalkduster")
+        gameOverLabel.text = (won ? "You Win!" : "You Lose")
+        gameOverLabel.fontSize = 50
+        gameOverLabel.fontColor = (won ? greenColor : SKColor.redColor())
+        gameOverLabel.position.x = size.width / 2.0
+        gameOverLabel.position.y = size.height / 2.0 + gameOverLabel.fontSize
+        
+        self.addChild(gameOverLabel)
+        
+        
+        
+        let elapsedTimeLabel = SKLabelNode(fontNamed: "Chalkduster")
+        elapsedTimeLabel.text = String(format: "Elapsed Time: %.1f", time)
+        elapsedTimeLabel.fontSize = 24
+        elapsedTimeLabel.fontColor = SKColor.blackColor()
+        elapsedTimeLabel.position.x = size.width / 2.0
+        elapsedTimeLabel.position.y = size.height / 2.0
+        
+        self.addChild(elapsedTimeLabel)
+        
+        
+        let newGamelabel = SKLabelNode(fontNamed: "Chalkduster")
+        newGamelabel.text = "Begin New Game"
+        newGamelabel.fontSize = 24
+        newGamelabel.fontColor = SKColor.greenColor()
+        newGamelabel.position.x = size.width / 2.0
+        newGamelabel.position.y = size.height / 2.0 - gameOverLabel.fontSize
+        
+        self.addChild(newGamelabel)
         
     }
     
@@ -22,6 +54,18 @@ class GameOverScene: SKScene {
     // not called, but required if you override SKScene's init
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    // present a new GameScene when user touches screen
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        let doorTransition = SKTransition.doorsOpenHorizontalWithDuration(1.0)
+        let scene = GameScene(size: self.size)
+        scene.scaleMode = .AspectFill
+        self.view?.presentScene(scene, transition: doorTransition)
+        
     }
 
 }
